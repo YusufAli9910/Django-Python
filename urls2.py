febdd7a7389bd8e3650b39bbd2be5e5fe5d6e63e
Yusuf Ali Okcu <105503884+YusufAli9910@users.noreply.github.com>
2026-01-17 00:56:53 +0000

    Add new file urls2.py

--- urls2.py
+++ urls2.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("<int:num1>", views.course_number_view, name='course_number_view'),#yusufaliokcu.com/first_app/10
     path("<str:item>" , views.course, name='course'),#yusufaliokcu.com/first_app/python
     path("<int:num1>/<int:num2>", views.calculator, name='calculator')#yusufaliokcu.com/first_app/5/4
+
 ]
